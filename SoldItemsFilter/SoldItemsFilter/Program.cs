f69e19bf7adc74927ef00c4973808ad9534ef179
Paul Macfarlane <pj16macf@siena.edu>
2020-05-25 23:46:12 -0400

    added quick helloworld program

---
+++ SoldItemsFilter/SoldItemsFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoldItemsFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
