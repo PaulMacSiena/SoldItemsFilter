2bf0a4bd231e8292813fe9833fa4d1229cb5e542
Paul Macfarlane <pj16macf@siena.edu>
2020-05-27 00:57:49 -0400

    main program/function completed

--- SoldItemsFilter/SoldItemsFilter/SoldItem.cs
+++ SoldItemsFilter/SoldItemsFilter/SoldItem.cs
@@ -13,9 +13,10 @@
         private int cost;
         private int salesPrice;
         private String salesPerson;
+        private int? salesPersonProfit;
 
         /*
-         * Constructor for the SoldItem Class
+         * Constructor for the SoldItem Class. salesPersonProfit gets initialized by static SetProfits(List<SoldItem> soldItems) method
          */
         public SoldItem(int id, String name, String serialNumber, int cost,
             int salesPrice, String salesPerson)
@@ -29,14 +30,28 @@
         }
 
         /*
-         * Implement the CompareTo Method so that a list of SoldItems can be sorted
+         * CompareTo Method for IComparable interface so that a list of SoldItems can be sorted (descending)
+         * in terms of profit
          */
         public int CompareTo(object obj)
         {
             if (obj is SoldItem)
             {
-                return this.name.CompareTo((obj as SoldItem).name);
-                //for now, SoldItems will be compared by the name attribute
+                if ((this.salesPrice-this.cost)- ((obj as SoldItem).salesPrice - (obj as SoldItem).cost)>0){
+                    return -1;
+                }
+                else if((this.salesPrice - this.cost) - ((obj as SoldItem).salesPrice - (obj as SoldItem).cost) < 0)
+                {
+                    return 1;
+                }
+                else if (this.serialNumber.CompareTo((obj as SoldItem).serialNumber) >0) // if the profitablity of items are the same, sort by serialnumber (descending)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
             }
             throw new ArgumentException("Object is not a SoldItem");
         }
@@ -48,37 +63,54 @@
         {
             return "id: " + this.id + ", name: " + this.name + ", serialNumber: " +
                 this.serialNumber + ", cost: " + this.cost + ", salesPrice: " +
-                this.salesPrice + ", salesPerson: " + this.salesPerson;
+                this.salesPrice + ", salesPerson: " + this.salesPerson + ", SalesPerson's Profit: " + this.salesPersonProfit;
         }
 
         /*
+         * Main Function for SoldItem Class First
          * Filters out items from list based off of 
          *  - if they are not profitable (if price < cost, there is no profit)
          *  - if the item was sold more than once
          *  - serial number consists of the capital letters of their name joined with their ID(punctuation and casing ignored)
+         *  After being filtered, the list is sorted by the most profitable
          */
         public static List<SoldItem> SoldItemsFilter(List<SoldItem> items)
         {
+            SetProfits(items); // static method that stores the profits of each salesPerson with each item
+
             items.RemoveAll(notProfitable); // remove all items not sold at a profit
 
             List<String> dupSerials = FindDuplicates(items); //get duplicate Serial Codes
 
-            List<SoldItem> itemsSoldOnce = new List<SoldItem>(); // create new list for items only sold once
+            List<SoldItem> itemSoldOnce= new List<SoldItem>(); // create new list for items only sold once
 
-            // add all items that were only sold once to list
+            // add all items that were only sold once to a new list
             foreach(SoldItem i in items)
             {
                 //if serial code was not duplicated, item was sold only 1 time
                 if (!dupSerials.Contains(i.serialNumber))
                 {
-                    itemsSoldOnce.Add(i);
+                    itemSoldOnce.Add(i);
                 }
             }
 
+            List<SoldItem> filteredItems = SerialFormat(itemSoldOnce); //filter out items without proper serial code
 
-            List<SoldItem> filteredItems = SerialFormat(itemsSoldOnce); //filter out items without proper serial code
+            filteredItems.Sort(); //sort the list from most profitable items to least profitable items
 
-            return filteredItems;
+            var query = filteredItems.GroupBy(item => item.salesPersonProfit); //group items by the profitability of its Sales Person
+
+            List<SoldItem> finalItems = new List<SoldItem>(); //create a final list to add filtered and sorted items to
+
+            foreach (var sItems in query)
+            {
+                foreach (var soldItem in sItems)
+                {
+                    finalItems.Add(soldItem);
+                }
+            }
+
+            return finalItems;
         }
 
         /*
@@ -148,13 +180,54 @@
 
         }
 
+        public static int CalculateSalesPersonProfit(String salesPersonName, List<SoldItem> items)
+        {
+            int profit = 0;
+
+            foreach(SoldItem item in items)
+            {
+                if (salesPersonName.Equals(item.salesPerson))
+                    profit = profit + (item.salesPrice - item.cost);
+            }
+
+            return profit;
+           
+        }
+
+        public static void SetProfits(List<SoldItem> soldItems)
+        {
+            List<String> salesPeople = new List<String>();
+            var query = soldItems.GroupBy(item => item.salesPerson);
+            foreach (var group in query)
+            {
+                foreach (var person in group)
+                {
+                    salesPeople.Add(person.salesPerson);
+                }
+            }
+            //Console.WriteLine("Original List: ");
+            foreach (String person in salesPeople)
+            {
+                int profit = CalculateSalesPersonProfit(person, soldItems);
+                //Console.WriteLine("Andy's Profit: " + CalculateSalesPersonProfit(person, soldItems));
+
+                //set person's profits everywhere
+                foreach (SoldItem item in soldItems)
+                {
+                    if (item.salesPerson.Equals(person))
+                    {
+                        item.salesPersonProfit = profit;
+                    }
+                }
+            }
+        }
+
         // Main Method to print output to console
-        public static void Main()
+        public static void Main(string[] args)
         {
 
-                // Initialize the original data
-                List<SoldItem> soldItems = new List<SoldItem>
-            { 
+            List<SoldItem> soldItems = new List<SoldItem>
+            {
                   new SoldItem(1,"Rock Salt", "RS1",10,50,"Andy Ghadban"),
                   new SoldItem(2,"Planter's Nuts", "XO28-V",4,23,"Reginald VelJohnson"),
                   new SoldItem(3,"Bulk Pack SuperWash Fire Hoses", "BPSW-FH3",122,122,"Harry Lewis"),
@@ -168,42 +241,23 @@
                   new SoldItem(11,"SwashBuckler's Buckled Swashes", "SBBS11",122,160,"Harry Lewis")
             };
 
-            
+            Console.WriteLine("Original List: "); 
+            foreach (SoldItem item in soldItems)
+            {
+                // Display Original List
+                Console.WriteLine(item.toString());
+            }
 
-                //List<String> dups = FindDuplicates(soldItems);
-                //foreach(String dup in dups)
-                //{
-                //    Console.WriteLine(dup);
-                //}
-                Console.WriteLine("Original List: ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Filtered and Sorted List: ");
 
-                foreach (SoldItem item in soldItems)
-                {
-                    // Display Original List
-                    Console.WriteLine(item.toString());
-                }
+            List<SoldItem> filteredAndSorted = SoldItemsFilter(soldItems);
 
-                Console.WriteLine("-----------");
-                Console.WriteLine("Filtered List: "); //only profit at this point
-
-                List<SoldItem> filteredItems = SoldItemsFilter(soldItems);
-
-                foreach (SoldItem item in filteredItems)
-                {
-                    // Display Filtered List
-                    Console.WriteLine(item.toString());
-                }
-
-                Console.WriteLine("-----------");
-                Console.WriteLine("Sorted List: ");
-                filteredItems.Sort();
-
-                foreach (SoldItem item in filteredItems)
-                {
-                    // Display Sorted List
-                    Console.WriteLine(item.toString());
-                }
-            
+            foreach (SoldItem item in filteredAndSorted)
+            {
+                // Display Sorted List
+                Console.WriteLine(item.toString());
+            }
 
         }
     }
